225828f45a3559b9e869ad86d753e38525bd9374
Johnnni7218 <johnnni@mail.ru>
2023-09-10 20:36:41 +0300

    Создание моделей и проведение миграций

--- Mounts/pereval/admin.py
+++ Mounts/pereval/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Coords, Mount, Photo, Level
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Coords)
+admin.site.register(Level)
+admin.site.register(Mount)
+admin.site.register(Photo)
