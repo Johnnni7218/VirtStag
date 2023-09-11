1a4090f6b972a0e0e41e6c99a547eb791f93b0ce
Johnnni7218 <johnnni@mail.ru>
2023-09-12 00:12:41 +0300

    Исправление Settings и Wiews

--- Mounts/pereval/views.py
+++ Mounts/pereval/views.py
@@ -4,21 +4,21 @@ from rest_framework import viewsets
 
 class CoordsViewSet(viewsets.ModelViewSet):
     queryset = Coords.objects.all()
-    serializers_class = CoordsSerializer
+    serializer_class = CoordsSerializer
 
 
 class LevelViewSet(viewsets.ModelViewSet):
     queryset = Level.objects.all()
-    serializers_class = LevelSerializer
+    serializer_class = LevelSerializer
 
 
 class PhotoViewSet(viewsets.ModelViewSet):
     queryset = Photo.objects.all()
-    serializers_class = PhotoSerializer
+    serializer_class = PhotoSerializer
 
 
 class MountViewSet(viewsets.ModelViewSet):
     queryset = Mount.objects.all()
-    serializers_class = MountSerializer
+    serializer_class = MountSerializer
 
 
