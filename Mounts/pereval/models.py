3e778415fd5c79c3452366e405bccee041252244
Johnnni7218 <johnnni@mail.ru>
2023-09-17 18:56:26 +0300

    Изменение с моделе Photo, поля ImageField на UrlField

--- Mounts/pereval/models.py
+++ Mounts/pereval/models.py
@@ -82,7 +82,7 @@ class Mount(models.Model):
 
 class Photo(models.Model):
     mount = models.ForeignKey(Mount, on_delete=models.CASCADE)
-    data = models.ImageField(upload_to='images/%Y-%m-%d/', verbose_name='Изображение', null=True)
+    data = models.URLField(verbose_name='Изображение', null=True, blank=True)
     title = models.CharField(max_length=255, verbose_name='Название')
 
     def __str__(self):
