b7c3b52ec1a08b2a82d828025da8af0d03f767af
Johnnni7218 <johnnni@mail.ru>
2023-09-22 21:12:31 +0300

    Переопределение метода Path в MountWiewset

--- Mounts/pereval/views.py
+++ Mounts/pereval/views.py
@@ -1,4 +1,5 @@
 import django_filters
+from rest_framework.response import Response
 
 from .serializers import *
 from rest_framework import viewsets
@@ -24,3 +25,30 @@ class MountViewSet(viewsets.ModelViewSet):
     serializer_class = MountSerializer
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
     filterset_fields = ["user__email"]
+
+    def record_update(self, request, pk, format=None):
+        record = self.get_object()
+        if record.status == 'new':
+            serializer = MountSerializer(record, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        'state': '1',
+                        'message': 'Изменения внесены успешно'
+                    }
+                )
+            else:
+                return Response(
+                    {
+                        'state': '0',
+                        'message': 'Ошибка'
+                    }
+                )
+        else:
+            return Response(
+                {
+                    'state': '0',
+                    'message': f'При данном статусе: {record.get_status_display()}, данные изменить нельзя!'
+                }
+            )
